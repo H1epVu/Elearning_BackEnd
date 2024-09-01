cdce2a6731951d1a3a6d3269fab3931866bfcdc1
H1ep Vu <hiepvu.tech02@gmail.com>
2024-09-01 19:59:39 +0700

    connect db

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const cors = require("cors")
 const bodyParser = require('body-parser')
 
+const sequelize = require('../db');
+const mongoose = require('mongoose');
+
 require('dotenv').config()
 
 const app = express();
@@ -12,6 +15,20 @@ app.use(bodyParser.json())
 
 app.use(cors({ origin: true }))
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
-})
+const connectDatabases = async () => {
+    try {
+        await Promise.all([
+            mongoose.connect(process.env.MONGODB_URI),
+            sequelize.sync({ force: true })
+        ]);
+        console.log('Connected to databases');
+
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}`);
+        });
+    } catch (err) {
+        console.error('Unable to connect to databases:', err);
+    }
+};
+
+connectDatabases();
