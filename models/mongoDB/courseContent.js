eda3b548229a5d2aaba122448fc964f6b587af3c
H1ep Vu <hiepvu.tech02@gmail.com>
2024-09-17 17:06:33 +0700

    updated section for course content

--- models/mongoDB/courseContent.js
+++ models/mongoDB/courseContent.js
@@ -2,19 +2,24 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema
 
+const LectureSchema = new Schema({
+    lecture_id: Schema.Types.ObjectId,
+    title: String,
+    video_url: String,
+});
+
+const SectionSchema = new Schema({
+    section_id: Schema.Types.ObjectId,
+    title: String,
+    lectures: [LectureSchema],
+});
+
 const CourseContentSchema = new Schema({
     course_id: {
         type: Number,
         required: true,
     },
-    lectures: [
-        {
-            lecture_id: Schema.Types.ObjectId,
-            title: String,
-            video_url: String,
-            duration: Number,
-        },
-    ],
+    sections: [SectionSchema],
 });
 
 module.exports = mongoose.model('CourseContent', CourseContentSchema);
